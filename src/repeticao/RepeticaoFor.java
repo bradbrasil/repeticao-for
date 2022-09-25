0b4a5518002bf525528128c734f254b6573e9536
bradbrasil <bradbrasil13@gmail.com>
2022-09-25 15:29:08 +0100

    repeticaofor

---
+++ src/repeticao/RepeticaoFor.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticao;
+
+/**
+ *
+ * @author bradb
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc=10; cc<=100; cc+=10;) {
+        System.out.println(cc);
+    
+    }
+        /*
+        int cc=0
+        while (cc<=4) {
+        system.out.println("cambalhota");
+        cc++;
+    }
+    
+}
